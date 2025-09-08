b7e41bdf01d80834ce698287a1b9b142cbc3a2cd
cato b <cato.benjaminsen@gmail.com>
2025-09-08 16:27:23 +0200

    Bio modal

--- js/app.js
+++ js/app.js
@@ -17,15 +17,14 @@ $(document).ready(function () {
   });
 
   // Modal functionality
-  $(".project-link-item[data-modal], .bio-link[data-modal]").on(
-    "click",
-    function (event) {
-      event.preventDefault();
-      var modalId = $(this).data("modal");
-      $("#" + modalId).fadeIn(300);
-      $("body").css("overflow", "hidden"); // Prevent background scrolling
-    }
-  );
+  $(
+    ".project-link-item[data-modal], .bio-link[data-modal], .bio-text-link[data-modal]"
+  ).on("click", function (event) {
+    event.preventDefault();
+    var modalId = $(this).data("modal");
+    $("#" + modalId).fadeIn(300);
+    $("body").css("overflow", "hidden"); // Prevent background scrolling
+  });
 
   // Close modal when clicking the X
   $(".close").on("click", function () {
